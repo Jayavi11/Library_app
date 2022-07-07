30fa34687db4ce0cc4f2d8068b6cad170a281347
Jayavi11 <jayavinayakjayakumar@gmail.com>
2022-07-07 22:22:55 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBooksComponent } from './add-books/add-books.component';
+import { DeleteBookComponent } from './delete-book/delete-book.component';
+import { SearchBookComponent } from './search-book/search-book.component';
+import { ViewBooksComponent } from './view-books/view-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBooksComponent,
+    DeleteBookComponent,
+    SearchBookComponent,
+    ViewBooksComponent
   ],
   imports: [
     BrowserModule,
